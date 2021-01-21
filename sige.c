2ccf360ec2df434e9a0672763b35d956d749f2c2
friedemannblanke <fblanke@protonmail.com>
2021-01-21 19:59:26 +0100

    modifications to add, non functional

--- sige.c
+++ sige.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #define DBPATH "./sigedb"
+//#define SITEDIR "./site"
 
 struct date {
 	int year;
@@ -18,35 +19,61 @@ struct post {
 	char filePath[200];
 };
 
+
+struct runOptions {
+	char outputFormat;
+	/* OPTIONS
+	 * q quiet
+	 * s standalone
+	 * a archive
+	 * f feed
+	 * r rss
+	 */
+};
+
 void addPost(struct post *newPost) {
-	FILE *dbptr;
-	dbptr = fopen(DBPATH, "a");
-	//writing to db requires turning date into readable format
-	fputs(newPost->author, dbptr);
-	fclose(dbptr);
+	FILE *dbPtr;
+	dbPtr = fopen(DBPATH, "a");
+	char dbBuffer[650];
+	//tab separated values, each post on its own line:
+	//url	title	year-month-day	author	filePath
+	sprintf(dbBuffer, "%s\t%s\t%d-%d-%d\t%s\t%s\n",
+		newPost->url,
+		newPost->title,
+		newPost->date.year,
+		newPost->date.month,
+		newPost->date.day,
+		newPost->author,
+		newPost->filePath
+	);
+	printf("%s", dbBuffer);
+	fclose(dbPtr);
 }
 
 
 void listPosts() {
-
+	FILE *dbptr;
+	dbptr = fopen(DBPATH, "r");
+	fclose(dbptr);
 }
 
 void removePost(char url[]) {
 
 }
 
+
 int main(int argc, char *argv[]) {
 	//parse CLI args
 	//first argument is command, following are options
 
 	struct post userProvided;
-	//options
+	//options & metadata
 	for (int i = 1; i < argc; i++) {
 		//detect flags
 		if (argv[i][0] == '-') {
 			switch (argv[i][1]) {
 				case 'd':
-					//date handling
+					//date handling, ISO 8601 formatted
 					sscanf(argv[i + 1], "%d-%d-%d", &userProvided.date.year, &userProvided.date.month, &userProvided.date.day);
 					break;
 				case 'a':
@@ -65,9 +92,14 @@ int main(int argc, char *argv[]) {
 					//file path handling
 					strcpy(userProvided.filePath, argv[i + 1]);
 					break;
+				case 'o':
+					//output format
+					break;
 				//uncaught cases are unrecognized flags
 				default:
 					printf("\x1b[33mWarning:\x1b[0m -%c is not a recognized option\n", argv[i][1]);
+					return 1;
+					break;
 			}
 			i++;
 		}
